724f9209de1e10ee793f4e43a87f9cd395851cae
Pedro Gaya <pedrogaya@gmail.com>
2021-08-30 00:22:55 -0300

    change size of body

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const rotasPedidos = require('./rotas/rotasPedidos');
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: '5mb' }));
+app.use(express.json({ limit: '20mb' }));
 app.use(rotasCategoria);
 app.use(rotasUsuario);
 app.use(rotasProduto);
